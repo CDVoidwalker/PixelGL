e39d21cdba9c92ff720e87896739bcaf211caccb
Wojciech Walaszek <wwalaszek02@gmail.com>
2021-01-03 22:01:23 +0100

    fixup

--- pixelGL/functions.h
+++ pixelGL/functions.h
@@ -11,7 +11,7 @@
 #include "pixel.h"
 namespace PixelGL
 {
-    void PXSleep(long long sleepMs)
+    static void PXSleep(long long sleepMs)
     {
 #ifdef __linux__
         usleep(sleepMs * 1000); // usleep takes sleep time in us (1 millionth of a second)
@@ -22,21 +22,21 @@ namespace PixelGL
     }
 
     // Copies number pixels from source to destination.
-    void memcpyPixel(Pixel *source, const size_t number, Pixel *destination)
+    static void memcpyPixel(Pixel *source, const size_t number, Pixel *destination)
     {
         for (size_t i = 0; i < number; i++)
             destination[i] = source[i];
     }
 
     // Sets number of pixels from start to constant color
-    void memsetPixel(Pixel *start, const size_t number, Pixel constant)
+    static void memsetPixel(Pixel *start, const size_t number, Pixel constant)
     {
         for (size_t i = 0; i < number; i++)
             start[i] = constant;
     }
 
     template <typename T>
-    T Clamp(T value, T low, T high)
+    static T Clamp(T value, T low, T high)
     {
         return (value < low) ? low : ((value > high) ? high : value);
     }
