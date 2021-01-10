f53d96e281d38f732175deeecbb25036516e93de
Wojciech Walaszek <wwalaszek02@gmail.com>
2021-01-10 01:58:04 +0100

    const correctness for vector2

--- pixelGL/vector2.h
+++ pixelGL/vector2.h
@@ -8,96 +8,89 @@ namespace PixelGL
     struct Vector2
     {
         T x, y;
-        Vector2()
-        {
-            x = T();
-            y = T();
-        }
 
-        Vector2(T x, T y)
-        {
-            this->x = x;
-            this->y = y;
-        }
+        Vector2() {this->x = T(), this->y = T()};
+        constexpr Vector2() : x(T()), y(T()) {};
+        constexpr Vector2(T x, T y) : x(x), y(y) {};
 
-        Vector2<T> operator+(const Vector2<T> &other)
+        constexpr Vector2<T> operator+(const Vector2<T> &other) const
         {
             return Vector2(x + other.x, y + other.y);
         }
 
-        Vector2<T> operator-(const Vector2<T> &other)
+        constexpr Vector2<T> operator-(const Vector2<T> &other) const
         {
             return Vector2(x - other.x, y - other.y);
         }
 
-        Vector2<T> operator*(const T &scale)
+        constexpr Vector2<T> operator*(const T &scale) const
         {
             return Vector2(x * scale, y * scale);
         }
 
-        Vector2<T> &operator+=(const Vector2<T> &other)
+        constexpr Vector2<T> &operator+=(const Vector2<T> &other)
         {
             this->x += other.x;
             this->y += other.y;
             return *this;
         }
 
-        Vector2<T> &operator-=(const Vector2<T> &other)
+        constexpr Vector2<T> &operator-=(const Vector2<T> &other)
         {
             this->x -= other.x;
             this->y -= other.y;
             return *this;
         }
 
-        bool operator==(const Vector2<T> &other) const
+        constexpr bool operator==(const Vector2<T> &other) const
         {
             return x == other.x && y == other.y;
         }
 
-        bool operator!=(const Vector2<T> &other) const
+        constexpr bool operator!=(const Vector2<T> &other) const
         {
             return !(*this == other);
         }
 
         // for sorting
-        bool operator<(const Vector2<T> &other) const
+        constexpr bool operator<(const Vector2<T> &other) const
         {
             return x < other.x || (x == other.x && y < other.y);
         }
 
-        T length() const
+        constexpr T length() const
         {
             return std::sqrt(x * x + y * y);
         }
 
-        void normalize() {
+        constexpr void normalize() {
             T lenreverse = 1.0 / length();
             this->x *= lenreverse;
             this->y *= lenreverse;
         }
 
-        void rotate(float rad) {
+        constexpr void rotate(float rad) {
             float s = sin(rad), c = cos(rad);
             this->x = x * c - y * s;
             this->y = x * s + y * c;
         }
 
-        operator Vector2<int>() const
+        constexpr operator Vector2<int>() const
         {
             return Vector2<int>((int)x, (int)y);
         }
 
-        operator Vector2<float>() const
+        constexpr operator Vector2<float>() const
         {
             return Vector2<float>((float)x, (float)y);
         }
 
-        operator Vector2<double>() const
+        constexpr operator Vector2<double>() const
         {
             return Vector2<double>((float)x, (float)y);
         }
 
-        operator Vector2<long>() const
+        constexpr operator Vector2<long>() const
         {
             return Vector2<long>((long)x, (long)y);
         }
